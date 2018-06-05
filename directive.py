1042ce9d06126224cbe5c595b995f3db58897ef2
Theo Cooper <tecnerd3@gmail.com>
2018-06-05 11:45:47 -0400

    wrote pseudocode for first steps of directive

--- directive.py
+++ directive.py
@@ -8,3 +8,17 @@
 # and diffusion parameters for matching 1D models to 3D data.
 
 from scipy.stats import chisquare
+import numpy as np
+
+#----Read in 3D simulation data----#
+
+# import [plotting script file]
+# read in 3D data into arrays using imported software
+# v_con_3D = 
+# r_sh_3D = 
+# Y_e_prof_3D = 
+# S_prof_3D = 
+
+#----Read in previous 1D simulation data----#
+
+# read in data from mixing-length parameter study for calibrating starting points
