a2f45319b0fdfc8fbadefd8f9e78536514564351
Brandon Barker <bbarker.py@protonmail.ch>
2018-06-05 11:34:12 -0400

    Added header to directive.py, imported scipy.stats.chisquare

---
+++ directive.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+# Script Name: directive.py
+# Version 0.0 (June 2018)
+# Authors: Theo Cooper and Brandon Barker
+#
+# Description: For use in paramter studies for the mixing length parameter 
+# and diffusion parameters for matching 1D models to 3D data.
+
+from scipy.stats import chisquare
