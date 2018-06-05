a9a8f637721832a66b3b1fcdeea8f10569335e14
Brandon Barker <bbarker.py@protonmail.ch>
2018-06-05 11:46:48 -0400

    ADded .gitignore file, some stuff to directive.py

--- directive.py
+++ directive.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+############################################################################
 #
 # Script Name: directive.py
 # Version 0.0 (June 2018)
@@ -6,5 +8,27 @@
 #
 # Description: For use in paramter studies for the mixing length parameter 
 # and diffusion parameters for matching 1D models to 3D data.
+#
+############################################################################
 
 from scipy.stats import chisquare
+import numpy as np
+import sys, os
+# import yt?
+# from optparse import OptionParser | For if we need commad line arguments
+
+# This "gets" the program name and assigns it to a variable.
+ScriptName = os.path.split(sys.argv[0])[1].split('.')[0]
+
+if __name__ == '__main__':
+    # Main program code goes in here
+
+    # Put initialization stuff here. Define timestep etc etc etc
+
+    # This prints the program name. Not necessary, put nice to do 
+    # before other output. Stylish. 
+    print('\n************************************')
+    print('** %s ' % ScriptName )
+    print('************************************\n')
+
+
