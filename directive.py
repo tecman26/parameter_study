0d60ed22cf0a752045d53aab7c9e5a6f4ac2de27
Brandon Barker <bbarker.py@protonmail.ch>
2018-06-05 12:04:19 -0400

    Minor Changes

--- directive.py
+++ directive.py
@@ -6,7 +6,7 @@
 # Version 0.0 (June 2018)
 # Authors: Theo Cooper and Brandon Barker
 #
-# Description: For use in paramter studies for the mixing length parameter 
+# Description: For use in paramter studies for the mixing length parameter
 # and diffusion parameters for matching 1D models to 3D data.
 #
 ############################################################################
@@ -15,32 +15,31 @@ from scipy.stats import chisquare
 import numpy as np
 import sys, os
 # import yt?
-# from optparse import OptionParser | For if we need commad line arguments
+# from optparse import OptionParser # For if we need commad line arguments
 
 # This "gets" the program name and assigns it to a variable.
 ScriptName = os.path.split(sys.argv[0])[1].split('.')[0]
 
 if __name__ == '__main__':
-    # Main program code goes in here
 
     # Put initialization stuff here. Define timestep etc etc etc
 
-    # This prints the program name. Not necessary, put nice to do 
-    # before other output. Stylish. 
+    # This prints the program name. Not necessary, put nice to do
+    # before other output. Stylish.
     print('\n************************************')
-    print('** %s ' % ScriptName )
+    print('**          %s ' % ScriptName )
     print('************************************\n')
 
-    
+
 
     #----Read in 3D simulation data----#
 
     # import [plotting script file]
     # read in 3D data into arrays using imported software
-    # v_con_3D = 
-    # r_sh_3D = 
-    # Y_e_prof_3D = 
-    # S_prof_3D = 
+    # v_con_3D =
+    # r_sh_3D =
+    # Y_e_prof_3D =
+    # S_prof_3D =
 
     #----Read in previous 1D simulation data----#
 
