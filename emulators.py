e6cd2d7bf423be81edd9903f3dbea2560e214b7b
Theo Cooper <f0004519@dev-intel16.i>
2018-06-22 11:14:22 -0400

    Added pickling and unpickling options to emulators.py

--- emulators.py
+++ emulators.py
@@ -8,16 +8,26 @@ import numpy as np
 from sklearn import gaussian_process
 from helper_functions import *
 from read1d import *
+import glob
+import pickle
 
 #initialize emulators
-r_sh_emul = gaussian_process.GaussianProcessRegressor(kernel="RBF(0.05)")
-v_con_emul = gaussian_process.GaussianProcessRegressor(kernel="RBF(0.05)")
-y_e_emul = gaussian_process.GaussianProcessRegressor(kernel="RBF(0.05)")
-s_emul = gaussian_process.GaussianProcessRegressor(kernel="RBF(0.05)")
+kernel_choice = gaussian_process.kernels.RBF(0.05)
+r_sh_file = "r_sh_emul_storage.pkl"
+v_con_file = "v_con_emul_storage.pkl"
+y_e_file = "y_e_emul_storage.pkl"
+s_file = "s_emul_storage.pkl"
 
-def calibrateEmulators(pos_pathname, data_dir): #positions file should have same format as positions.txt
+r_sh_emul = gaussian_process.GaussianProcessRegressor(kernel=kernel_choice)
+v_con_emul = gaussian_process.GaussianProcessRegressor(kernel=kernel_choice)
+y_e_emul = gaussian_process.GaussianProcessRegressor(kernel=kernel_choice)
+s_emul = gaussian_process.GaussianProcessRegressor(kernel=kernel_choice)
+
+def calibrateEmulators(data_dir): #positions file should have same format as positions.txt
     #data directory contains all run directories from calibration run
-    pos_arr = np.array(readPositions(pos_pathname)[0])
+    pos_pathname = os.path.join(data_dir, "positions.txt")
+    pos_dict, pos_arr = readPositions(pos_pathname)
+    pos_arr = np.array(pos_arr)
     num_samples = pos_arr.shape[0]
     
     #list of radius values, for reference
@@ -29,7 +39,7 @@ def calibrateEmulators(pos_pathname, data_dir): #positions file should have same
     y_e_arr = []
     s_arr = []
     
-    for i in range(1,num_samples+1)
+    for i in range(1,num_samples+1):
         data_pathname = glob.glob(os.path.join(data_dir,"run_mcmcPS_"+str(i)+"*"))
         data_pathname = data_pathname[0]
         
@@ -48,12 +58,39 @@ def calibrateEmulators(pos_pathname, data_dir): #positions file should have same
     y_e_arr = np.array(y_e_arr)
     s_arr = np.array(s_arr)
     
-    
     r_sh_emul.fit(pos_arr,r_sh_emul)
     v_con_emul.fit(pos_arr,v_con_arr)
     y_e_emul.fit(pos_arr,y_e_arr)
     s_emul.fit(pos_arr,s_emul)
+
+def storeEmulators(file1=r_sh_file, file2=v_con_file, file3=y_e_file, file4=s_emul_file):
+    with open(file1, "w+b") as f1:
+        pickle.dump(r_sh_emul, f1, pickle.HIGHEST_PROTOCOL)
+    with open(file2, "w+b") as f2: 
+        pickle.dump(v_con_emul, f2, pickle.HIGHEST_PROTOCOL)
+    with open(file3, "w+b") as f3: 
+        pickle.dump(y_e_emul, f2, pickle.HIGHEST_PROTOCOL)
+    with open(file2, "w+b") as f4: 
+        pickle.dump(s_emul, f4, pickle.HIGHEST_PROTOCOL)
     
+def loadEmulators(file1=r_sh_file, file2=v_con_file, file3=y_e_file, file4=s_emul_file):
+    r_sh_load = gaussian_process.GaussianProcessRegressor(kernel=kernel_choice)
+    v_con_load = gaussian_process.GaussianProcessRegressor(kernel=kernel_choice)
+    y_e_load = gaussian_process.GaussianProcessRegressor(kernel=kernel_choice)
+    s_load = gaussian_process.GaussianProcessRegressor(kernel=kernel_choice)
+
+
+    with open(file1, "r+b") as f1:
+        r_sh_load = pickle.load(f1)
+    with open(file2, "r+b") as f2: 
+        v_con_load = pickle.load(f2)
+    with open(file3, "r+b") as f3: 
+        y_e_load = pickle.load(f3)
+    with open(file2, "r+b") as f4: 
+        s_load = pickle.load(f4)
+ 
+    return r_sh_load, v_con_load, y_e_load, s_load
+
 def radii():
     return radius_ref
     
@@ -68,3 +105,7 @@ def emulYE(arr):
 
 def emulS(arr):
     return s_emul.predict(arr,return_std=True)
+
+
+calibrateEmulators(trial_directory)
+storeEmulators()
